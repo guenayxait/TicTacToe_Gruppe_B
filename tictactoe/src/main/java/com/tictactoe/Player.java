c0206308f6028d34fa25f27eccfeb1e42fe846c3
Réka Reményi <remenyireka2000@gmail.com>
2024-05-25 01:06:46 +0200

    added the (almost) empty base classes

---
+++ tictactoe/src/main/java/com/tictactoe/Player.java
@@ -0,0 +1,16 @@
+package main.java.com.tictactoe;
+
+public class Player {
+    // variables
+    char marker;
+
+    // constructor
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+    // methods
+    public char getMarker() {
+        return marker;
+    }
+}
