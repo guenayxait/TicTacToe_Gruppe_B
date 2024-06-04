92fe3ae6c2b994124de15c5fcb6beb2284a22ac5
aylin <guenay@artindustrial-it.com>
2024-06-05 00:11:37 +0200

    Added testcase and updated pom.xml

---
+++ src/test/java/com/tictactoe/TicTacToeTest.java
@@ -0,0 +1,34 @@
+package com.tictactoe;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class TicTacToeTest {
+    @Test
+    public void testIsCellEmpty() {
+        Board board = new Board();
+        assertTrue(board.isCellEmpty(0, 0));
+        assertTrue(board.isCellEmpty(1, 1));
+        assertTrue(board.isCellEmpty(2, 2));
+        board.place(0, 0, 'X');
+        assertFalse(board.isCellEmpty(0, 0));
+        assertTrue(board.isCellEmpty(1, 1));
+        assertTrue(board.isCellEmpty(2, 2));
+    }
+
+    @Test
+    public void testSwitchCurrentPlayer() {
+        Player player1 = new Player('X');
+        Player player2 = new Player('O');
+        Player currentPlayer = player1;
+        assertEquals(player1, currentPlayer);
+        currentPlayer = player2;
+        assertEquals(player2, currentPlayer);
+    }
+
+
+    public static void main(String[] args) {
+        org.junit.runner.JUnitCore.main("com.tictactoe.TicTacToeTest");
+    }
+}
