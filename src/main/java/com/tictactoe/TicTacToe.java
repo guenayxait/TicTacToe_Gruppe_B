e66d84b7434ce241e8a6014c2c2eb95bb6c9a34d
HoKipsy <remenyireka2000@gmail.com>
2024-06-30 01:06:17 +0200

    start() method implemented

--- src/main/java/com/tictactoe/TicTacToe.java
+++ src/main/java/com/tictactoe/TicTacToe.java
@@ -4,55 +4,35 @@ import java.util.Scanner; // Import the Scanner class
 
 public class TicTacToe {
     // variables:
-    static Player player1 = new Player('X');
-    static Player player2 = new Player('O');
-    static Player currentPlayer = player1;
-    static Board board = new Board();
+    static Player player1;
+    static Player player2;
+    static Player currentPlayer;
+    static Board board;
 
     // Create Main Method
     public static void main(String[] args) {
-        TicTacToe game = new TicTacToe();
-        //game.start();
+        boolean gameOngoing = true;
+        Scanner sc = new Scanner(System.in);
+        String answer;
 
-        System.out.println("Current Player: " + currentPlayer.getMarker());
+        while (gameOngoing) {
+            start();
+            System.out.println("Do you want to start a new game? (y/n)");
+            answer = sc.nextLine();
+            do {
+                if (answer.equals("n")) {
+                    gameOngoing = false;
+                } else if (!answer.equals("y")) {
+                    System.out.println("Invalid input");
+                }
+            } while (!answer.equals("n") && !answer.equals("y"));
 
-        // print empty board
-        board.print();
-        // while loop
-        while (true) {
-            // get input from user
-            System.out.println("row (0-2): ");
-            Scanner scanner = new Scanner(System.in);
-            int row = scanner.nextInt();
-            System.out.println("col (0-2): ");
-            int col = scanner.nextInt();
-            // check if cell is empty
-            if (board.isCellEmpty(row, col)) {
-                // place marker on the board
-                board.place(row, col, currentPlayer.getMarker());
-                // print board
-                board.print();
-                // check if there is a winner
-                if (hasWinner()) {
-                    System.out.println("Player " + currentPlayer.getMarker() + " wins!");
-                    break;
-                }
-                // check if board is full
-                if (board.isFull()) {
-                    System.out.println("It's a tie!");
-                    break;
-                }
-                // switch current player
-                switchCurrentPlayer();
-                System.out.println("Current Player: " + currentPlayer.getMarker());
-            } else {
-                System.out.println("Cell is not empty!");
-            }
         }
+
+
     }
 
     private static void switchCurrentPlayer() {
-
         if (currentPlayer == player1) {
             currentPlayer = player2;
         } else {
@@ -76,7 +56,53 @@ public class TicTacToe {
                 (board.cells[0][2] == check && board.cells[1][1] == check && board.cells[2][0] == check);
     }
 
-    //private void start() {}
+    public static void start() {
+        player1 = new Player('X');
+        player2 = new Player('O');
+        currentPlayer = player1;
+        board = new Board();
+
+        boolean gameOngoing = true;
+
+        // print empty board
+        board.print();
+
+        Scanner scanner = new Scanner(System.in);
+        int row;
+        int col;
+
+        // while loop
+        while (gameOngoing) {
+            System.out.println("Current Player: " + currentPlayer.getMarker());
+            // get input from user
+            System.out.print("row (0-2): ");
+            row = scanner.nextInt();
+            System.out.print("col (0-2): ");
+            col = scanner.nextInt();
+            // check if cell is empty
+            if (board.isCellEmpty(row, col)) {
+                // place marker on the board
+                board.place(row, col, currentPlayer.getMarker());
+                // print board
+                board.print();
+                // check if there is a winner
+                if (hasWinner()) {
+                    System.out.println("Player " + currentPlayer.getMarker() + " wins!");
+                    gameOngoing = false;
+                }
+                // check if board is full
+                else if (board.isFull()) {
+                    System.out.println("It's a tie!");
+                    gameOngoing = false;
+                }
+                // switch current player
+                switchCurrentPlayer();
+
+            } else {
+                System.out.println("Cell is not empty!");
+            }
+        }
+    }
 
 
 
