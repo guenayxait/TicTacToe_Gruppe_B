2739359928ba7305bd842485271ff6bbbe3b185b
Roger <rogerwilhelm@hotmail.de>
2024-06-30 13:44:54 +0200

    Testing implemented for row, column, diagonals and no win cases.

--- src/test/java/com/tictactoe/TicTacToeTest.java
+++ src/test/java/com/tictactoe/TicTacToeTest.java
@@ -26,4 +26,61 @@ public class TicTacToeTest {
         currentPlayer = player2;
         assertEquals(player2, currentPlayer);
     }
+
+    @Test
+    public void testHasWinner_rowWin() {
+        // Set up a winning condition in the first column
+        TicTacToe.board.place(0,0,'X');
+        TicTacToe.board.place(0,1,'X');
+        TicTacToe.board.place(0,2,'X');
+
+        assertTrue(TicTacToe.hasWinner());
+    }
+
+    @Test
+    public void testhasWinner_columnWin(){
+        // Set up winning condition in the first column
+        TicTacToe.board.place(0,0,'X');
+        TicTacToe.board.place(1,0,'X');
+        TicTacToe.board.place(2,0,'X');
+
+        assertTrue(TicTacToe.hasWinner());
+    }
+
+    @Test
+    public void testhasWinner_diagonalWin(){
+        // Set up a winning condition in the main diagonal
+        TicTacToe.board.place(0,0,'X');
+        TicTacToe.board.place(1,1,'X');
+        TicTacToe.board.place(2,2,'X');
+
+        assertTrue(TicTacToe.hasWinner());
+    }
+
+    @Test
+    public void testhasWinner_antiDiagonalWin(){
+        // Set up a winning condition in the anti-diagonal
+        TicTacToe.board.place(0,2,'X');
+        TicTacToe.board.place(1,1,'X');
+        TicTacToe.board.place(2,0,'X');
+
+        assertTrue(TicTacToe.hasWinner());
+    }
+
+    @Test
+    public void testHasWinner_noWin(){
+        // Set up a condition with no winner
+        TicTacToe.board.place(0,0,'X');
+        TicTacToe.board.place(0,1,'O');
+        TicTacToe.board.place(0,2,'X');
+        TicTacToe.board.place(0,0,'X');
+        TicTacToe.board.place(0,1,'O');
+        TicTacToe.board.place(0,2,'O');
+        TicTacToe.board.place(0,0,'O');
+        TicTacToe.board.place(0,1,'X');
+        TicTacToe.board.place(0,2,'O');
+
+        assertFalse(TicTacToe.hasWinner());
+    }
+
 }
